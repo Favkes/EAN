e9a01d7da80c1698284a4eab1b8f269952cf64af
Favkes <tho.strongest.avenger@gmail.com>
2025-08-20 18:58:06 +0200

    Revert "Add newline character ignore in all parser functions"
    This reverts commit 67ae86cfa087e3917f49f12b784ff31028a44992.

--- gui/functional_gui.py
+++ gui/functional_gui.py
@@ -5,27 +5,9 @@ class InputGUI(tk.Frame):
     def __init__(self, root_frame: tk.Frame):
         super().__init__(root_frame)
         self.root_frame = root_frame
-        self.config(
-            padx=15
-        )
 
-        self.input_field_title = tk.Label(
-            self,
-            text="Input data:"
-        )
-        self.input_field_var = tk.StringVar()
-        self.input_field_entry = tk.Entry(
-            self,
-            textvariable=self.input_field_var
-        )
-
-    def build(self):
-        self.input_field_title.grid(
-            row=0, column=0, sticky='w'
-        )
-        self.input_field_entry.grid(
-            row=1, column=0, sticky='w'
-        )
+        self.label = tk.Label(self, text="GUI")
+        self.label.pack()
 
 
 if __name__ == "__main__":
