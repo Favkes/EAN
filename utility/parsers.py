e54513ae1b188d47a1efd2095165ed5cb60c668e
Favkes <tho.strongest.avenger@gmail.com>
2025-08-22 21:48:33 +0200

    Make float-imitation formatting optional, controlled from the MAKE_FLOATY global at utility.parsers

--- utility/parsers.py
+++ utility/parsers.py
@@ -3,6 +3,7 @@ from mpmath import iv, mp
 
 iv.prec = 64
 mp.prec = 64
+MAKE_FLOATY = False     # Formats all numbers with trailing zeros and adds e+0 if there's no exponent.
 
 
 def parse_real(s: str) -> list[float]:
@@ -43,11 +44,33 @@ def safe_parse(parser_func,
         return None
 
 
+def always_look_like_float(strnum: str) -> str:
+    if 'e' not in strnum:
+        return f'{strnum:018s}' + 'e+0'
+    return strnum
+
+
 def prettify(data: iv.mpf | mp.mpf) -> str:
-    output = str(data)
-    if 'e' not in output:
-        output = f'{output:018s}' + 'e+0'
-    return output
+    if isinstance(data, mp.mpf):
+        output = str(data)
+
+        if MAKE_FLOATY:
+            output = always_look_like_float(output)
+
+        return output
+
+    elif isinstance(data, iv.mpf):
+        output = str(data)
+
+        if MAKE_FLOATY:
+            outA, outB = output[1:-1].split(', ')
+            outA = always_look_like_float(outA)
+            outB = always_look_like_float(outB)
+            output = f'[{outA}, {outB}]'
+
+        return output
+
+    else: return 'Type error at utility.parsers.prettify():\n'+str(type(data))+' is an incorrect type.'
 
 
 if __name__ == "__main__":
