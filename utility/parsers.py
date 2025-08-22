ac19e0b735ad939419ff48b5197ca466315aa801
Favkes <tho.strongest.avenger@gmail.com>
2025-08-22 15:27:11 +0200

    Correct all utility.parsers functions by swapping int() to mp.mpf(), set correct precision to 64 for both mp and iv.

--- utility/parsers.py
+++ utility/parsers.py
@@ -1,21 +1,25 @@
-from mpmath import iv
+from mpmath import iv, mp
 
 
-def parse_real(s: str):
+iv.prec = 64
+mp.prec = 64
+
+
+def parse_real(s: str) -> list[float]:
     s = s.replace(' ', '').replace('\n', '')
     args = s.split(',')
-    return [int(x) for x in args]
+    return [mp.mpf(x) for x in args]
 
 
-def parse_singleton(s: str):
+def parse_singleton(s: str) -> list:
     return [iv.mpf([x, x]) for x in parse_real(s)]
 
 
-def parse_interval(s: str):
+def parse_interval(s: str) -> list:
     s = s.replace(' ', '').replace('\n', '')
     s = s.replace('[', '').replace(']', '').replace(';', ',')
     args = s.split(',')
-    args = [int(x) for x in args]
+    args = [mp.mpf(x) for x in args]
     return [
         iv.mpf([
             min(args[i], args[i+1]),
@@ -32,5 +36,10 @@ if __name__ == "__main__":
     print(
         parse_singleton(
             "1, 2, 5, 6"
+        )
+    )
+    print(
+        parse_real(
+            "2, 3, 5,7, 11"
         )
     )
