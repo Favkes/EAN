45624acf07678610ade3058a012e2ddbfbd4f0cf
Favkes <tho.strongest.avenger@gmail.com>
2025-08-22 17:31:50 +0200

    Implement a simple mathematical operation inside the lagrange function

--- utility/algorithm.py
+++ utility/algorithm.py
@@ -1,11 +1,15 @@
 from mpmath import iv, mp
 
+
 iv.prec = 64
 mp.prec = 64
 
 
 def lagrange(x, y, z) -> mp.mpf | iv.mpf:
-    return x
+    out = []
+    for x_, y_ in zip(x, y):
+        out.append(x_ + y_)
+    return out
 
 
 def neville() -> mp.mpf | iv.mpf:
