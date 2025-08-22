4a02196340a3b482655053a8f78fc588744eb243
Favkes <tho.strongest.avenger@gmail.com>
2025-08-22 19:59:33 +0200

    Implement the Lagrange's interpolation polynomial algorithm in utility.algorithm.lagrange()

--- utility/algorithm.py
+++ utility/algorithm.py
@@ -6,10 +6,19 @@ mp.prec = 64
 
 
 def lagrange(x, y, z) -> mp.mpf | iv.mpf:
-    out = []
-    for x_, y_ in zip(x, y):
-        out.append(x_ + y_)
-    return out
+    dtype = type(x[0])
+    result = dtype(0)
+    for i in range(len(x)):
+        term = y[i]
+        for j in range(len(x)):
+            if i == j: continue
+            numerator = z[0] - x[j]
+            denominator = x[i] - x[j]
+            fraction = numerator / denominator
+
+            term = term * fraction
+        result = result + term
+    return result
 
 
 def neville() -> mp.mpf | iv.mpf:
