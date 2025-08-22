4de6fe4214bee16f3b68fcc6bb8dcfcb6c484ae1
Favkes <tho.strongest.avenger@gmail.com>
2025-08-22 21:28:25 +0200

    Add binary content check for lagrange() function result (print in terminal)

--- utility/algorithm.py
+++ utility/algorithm.py
@@ -18,6 +18,8 @@ def lagrange(x, y, z) -> mp.mpf | iv.mpf:
 
             term = term * fraction
         result = result + term
+    sgn, man, exp, bic = result._mpf_
+    print(sgn, bin(man)[2:], exp, bic)
     return result
 
 
