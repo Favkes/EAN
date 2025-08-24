37fdf1abf0d0a3ef780eb11b020b47c05fce9df5
Favkes <tho.strongest.avenger@gmail.com>
2025-08-24 17:23:59 +0200

    Implement Neville's algorithm inside utility.algorithm.neville()

--- utility/algorithm.py
+++ utility/algorithm.py
@@ -21,5 +21,15 @@ def lagrange(x, y, z) -> mp.mpf | iv.mpf:
     return result
 
 
-def neville() -> mp.mpf | iv.mpf:
-    return None
+def neville(x, y, z) -> mp.mpf | iv.mpf:
+    y_data = y.copy()
+
+    for i in range(1, len(x)):
+        for j in range(len(x) - i):
+            dx_ij = z - x[i + j]
+            dx_j = x[j] - z
+            dx_j_ij = x[j] - x[i + j]
+            print(i, j)
+
+            y[j] = (dx_ij * y[j] + dx_j * y[j + 1]) / dx_j_ij
+    return y[0]
