0b2f4220cbe30db6fd30e447615e0762ac8be3c4
Favkes <tho.strongest.avenger@gmail.com>
2025-08-24 20:04:42 +0200

    Implement utility.algorithm.lagrange_coefficients() function and add it to gui.core.App.calculate_button_func(). Introduce minor fixes inside neville()

--- utility/algorithm.py
+++ utility/algorithm.py
@@ -22,7 +22,7 @@ def lagrange(x, y, z) -> mp.mpf | iv.mpf:
 
 
 def neville(x, y, z) -> mp.mpf | iv.mpf:
-    y_data = y.copy()
+    y_ = y.copy()
     z = z[0]
 
     for i in range(1, len(x)):
@@ -30,7 +30,33 @@ def neville(x, y, z) -> mp.mpf | iv.mpf:
             dx_ij = z - x[i + j]
             dx_j = x[j] - z
             dx_j_ij = x[j] - x[i + j]
-            print(i, j)
 
-            y[j] = (dx_ij * y[j] + dx_j * y[j + 1]) / dx_j_ij
-    return y[0]
+            y_[j] = (dx_ij * y_[j] + dx_j * y_[j + 1]) / dx_j_ij
+    return y_[0]
+
+
+def lagrange_coefficients(x, y) -> list[mp.mpf | iv.mpf]:
+    dtype = type(x[0])
+    out = [dtype(0)] * len(x)
+
+    for i in range(len(x)):
+        coeffs = [dtype(1)]
+        denominator = dtype(1)
+
+        for j in range(len(x)):
+            if i == j: continue
+
+            coeffs_tmp = [dtype(0)] * (len(coeffs) + 1)
+
+            for k in range(len(coeffs)):
+                coeffs_tmp[k]     -= coeffs[k] * x[j]
+                coeffs_tmp[k + 1] += coeffs[k]
+
+            coeffs = coeffs_tmp
+            denominator *= x[i] - x[j]
+
+        scale = y[i] / denominator
+        for j in range(len(coeffs)):
+            out[j] += coeffs[j] * scale
+
+    return out
