d38fec4ce1badc86e6c4004001c936aeb08b67ff
Favkes <tho.strongest.avenger@gmail.com>
2025-08-25 00:39:47 +0200

    Add sphinx docstring headers to main.py, change the docs structure to resemble the project structure more closely

---
+++ docs/gui/index.rst
@@ -0,0 +1,9 @@
+gui package
+===========
+
+
+.. toctree::
+    :maxdepth: 2
+    :caption: Contents:
+
+    core
--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+"""
+Main running file of the app.
+
+Contains the definition of the app's root window and initialization of core.App, that further handles everything.
+"""
+
+
 from gui.core import App
 import tkinter as tk
 
 
 def main():
+    """
+
+
+    :return: None
+    """
     root = tk.Tk()
     root.geometry("700x500")
     root.minsize(400, 300)
