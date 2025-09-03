e246ad44617e68f1ee6aedfc7610aeef39ec75e3
Favkes <tho.strongest.avenger@gmail.com>
2025-09-03 16:04:21 +0200

    Rename all parameters in utility.algorithm from x to arr_x, y to arr_y and z to arr_z

--- utility/algorithm.py
+++ utility/algorithm.py
@@ -20,15 +20,15 @@ iv.prec = 64
 mp.prec = 64
 
 
-def lagrange(x, y, z) -> mp.mpf | iv.mpf:
-    dtype = type(x[0])
+def lagrange(arr_x, arr_y, arr_z) -> mp.mpf | iv.mpf:
+    dtype = type(arr_x[0])
     result = dtype(0)
-    for i in range(len(x)):
-        term = y[i]
-        for j in range(len(x)):
+    for i in range(len(arr_x)):
+        term = arr_y[i]
+        for j in range(len(arr_x)):
             if i == j: continue
-            numerator = z[0] - x[j]
-            denominator = x[i] - x[j]
+            numerator = arr_z[0] - arr_x[j]
+            denominator = arr_x[i] - arr_x[j]
             fraction = numerator / denominator
 
             term = term * fraction
@@ -36,41 +36,41 @@ def lagrange(x, y, z) -> mp.mpf | iv.mpf:
     return result
 
 
-def neville(x, y, z) -> mp.mpf | iv.mpf:
-    y_ = y.copy()
-    z = z[0]
+def neville(arr_x, arr_y, arr_z) -> mp.mpf | iv.mpf:
+    y_ = arr_y.copy()
+    arr_z = arr_z[0]
 
-    for i in range(1, len(x)):
-        for j in range(len(x) - i):
-            dx_ij = z - x[i + j]
-            dx_j = x[j] - z
-            dx_j_ij = x[j] - x[i + j]
+    for i in range(1, len(arr_x)):
+        for j in range(len(arr_x) - i):
+            dx_ij = arr_z - arr_x[i + j]
+            dx_j = arr_x[j] - arr_z
+            dx_j_ij = arr_x[j] - arr_x[i + j]
 
             y_[j] = (dx_ij * y_[j] + dx_j * y_[j + 1]) / dx_j_ij
     return y_[0]
 
 
-def lagrange_coefficients(x, y) -> list:
-    dtype = type(x[0])
-    out = [dtype(0)] * len(x)
+def lagrange_coefficients(arr_x, arr_y) -> list:
+    dtype = type(arr_x[0])
+    out = [dtype(0)] * len(arr_x)
 
-    for i in range(len(x)):
+    for i in range(len(arr_x)):
         coeffs = [dtype(1)]
         denominator = dtype(1)
 
-        for j in range(len(x)):
+        for j in range(len(arr_x)):
             if i == j: continue
 
             coeffs_tmp = [dtype(0)] * (len(coeffs) + 1)
 
             for k in range(len(coeffs)):
-                coeffs_tmp[k]     -= coeffs[k] * x[j]
+                coeffs_tmp[k]     -= coeffs[k] * arr_x[j]
                 coeffs_tmp[k + 1] += coeffs[k]
 
             coeffs = coeffs_tmp
-            denominator *= x[i] - x[j]
+            denominator *= arr_x[i] - arr_x[j]
 
-        scale = y[i] / denominator
+        scale = arr_y[i] / denominator
         for j in range(len(coeffs)):
             out[j] += coeffs[j] * scale
 
