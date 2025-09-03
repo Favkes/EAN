c9dbdfefe8dbfa2932ef9f706e65d80ec793c215
Favkes <tho.strongest.avenger@gmail.com>
2025-09-04 01:11:38 +0200

    Add file docs header string to utility.parsers

--- utility/parsers.py
+++ utility/parsers.py
@@ -1,3 +1,24 @@
+"""
+Data extraction module containing all the parsing functions used to convert user input into actual numeric data.
+
+This module contains several parsing and formatting functions used in the gui.core module to convert
+string numbers into actual mpmath numeric objects for all three arithmetic modes, as well as a means of
+safely embedding these parsing functions to error-proof the application. There are also functions used
+for formatting the output data right before writing it to the output widget.
+
+Functions:
+    parse_real(s): Converts numeric array string into an mp.mpf list
+    parse_singleton(s): Converts numeric array string into a list of iv.mpf singleton intervals `[a; a]`
+    parse_interval(s): Converts numeric interval array string into a list of iv.mpf intervals `[a; b]`
+    safe_parse(parser_func, data_str, error_command, error_message): Embeds execution of the parser_func function
+    in an exception block, allowing for configurable error feedback display directly to the output widget.
+    sci_str(x, prec): Converts an mp.mpf number into a numeric string in the scientific format, with defined
+    digital precision.
+    prettify(data): Formats the output data of any of the 3 algorithms implemented in the project into human-readable
+    text ready to be printed in the output widget.
+"""
+
+
 from mpmath import iv, mp
 
 
@@ -64,7 +85,7 @@ def sci_str(x, prec: int = 18):
     return f"{mant_str}E{exp_str}"
 
 
-def prettify(data: iv.mpf | mp.mpf) -> str:
+def prettify(data: iv.mpf | mp.mpf | list) -> str:
     if isinstance(data, mp.mpf):
         output = sci_str(data)
         return output
