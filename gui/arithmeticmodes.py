b79ec72ab99637c9134fc2fdfa0008c9caeb4df9
Favkes <tho.strongest.avenger@gmail.com>
2025-09-03 17:03:07 +0200

    Change order of variable definitions inside gui.arithmeticmodes

--- gui/arithmeticmodes.py
+++ gui/arithmeticmodes.py
@@ -29,13 +29,13 @@ input_placeholder_text_map: dict = {
     'interval':     '[0;1],\n[2.34; 5], [6.7, 8]',
     'singleton':    '0,\n1,2, 3'
 }
-input_allowed_chars_map: dict = {
-    'real':         '-0.123,456;789 \n',
-    'interval':     '-0.123,456;789[] \n',
-    'singleton':    '-0.123,456;789 \n'
-}
 input_new_point_placeholder_text_map: dict = {
     'real':         '3.14159265359',
     'interval':     '[0;3.14159265359]',
     'singleton':    '3.14159265359'
+}
+input_allowed_chars_map: dict = {
+    'real':         '-0.123,456;789 \n',
+    'interval':     '-0.123,456;789[] \n',
+    'singleton':    '-0.123,456;789 \n'
 }
